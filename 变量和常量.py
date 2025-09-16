d60a695f006ce5e07cbce272e106db2d67432103
hu cheng jian <13766397710@139.com>
2025-09-16 22:42:37 +0800

    第二次用《pytho:从入门到实践》学习的一些程序。好玩又有趣。

---
+++ 变量和常量.py
@@ -0,0 +1,15 @@
+#同时给多个变量赋值：
+di_yi,di_er,di_san=123,456,789
+print(di_yi)
+print(di_er)
+print(di_san)
+
+
+#常量是在程序整个生命周期内保持不变的变量，其变量名用大写字母表示。
+
+NAME='HU CHENG JIAN'
+print(NAME)
+#重新对常量赋值，常量不会保持不变。
+#这是因为常量是人为认定的不能变得变量。
+NAME='123'
+print(NAME)
