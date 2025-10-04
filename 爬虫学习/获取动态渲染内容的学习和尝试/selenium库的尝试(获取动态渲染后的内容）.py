6b929e40e4ac25ad18983dd157f823f7c73583fd
hu cheng jian <13766397710@139.com>
2025-10-04 20:10:28 +0800

    我能获取动态渲染的内容了。

--- 爬虫学习/获取动态渲染内容的学习和尝试/selenium库的尝试(获取动态渲染后的内容）.py
+++ 爬虫学习/获取动态渲染内容的学习和尝试/selenium库的尝试(获取动态渲染后的内容）.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
+from bs4 import BeautifulSoup#一个傻瓜式的解析库
 '''selenium.webdriver.edge：这是webdriver包中专门用于Edge浏览器的子包
 
 options：这是edge子包中的一个模块
@@ -30,7 +31,8 @@ Edge()：创建Edge类的实例（一个具体的浏览器控制对象）'''
 
 url='https://movie.douban.com/chart'#一个用来储存url的变量
 driver.get(url)
-print(driver.page_source)# 调用driver实例的page_source属性，获取页面HTML源码
+#print(driver.page_source)
+#调用driver实例的page_source属性，获取页面HTML源码
 #对象名.属性   经典的获取对象的属性的操作，明明在林莉莉的网课上已经学过，但是看到这个还是没认出来，没理解到位，真是经验不足啊。要多加练习啊，
 #在ai给出的大量代码示例中，锻练自己的代码分析和识别能力。
 
@@ -39,3 +41,9 @@ print(driver.page_source)# 调用driver实例的page_source属性，获取页面
 '''成功了，我成功了！我终于获取到了经过动态渲染后的html源代码，今天是2025年10月4日，是一个里程碑式的日子。
 虽然这段代码还有一些不理解的，但是还是好开心，因为这意味着我的爬虫能力上了一个新台阶！
 '''
+
+html=driver.page_source
+soup=BeautifulSoup(html,'html.parser')
+v=soup.find_all('span')
+for x in v:
+    print(x.string)
