20de6070fa6750fe540d0e7a0390a36f1bce67ca
hu cheng jian <13766397710@139.com>
2025-10-05 13:13:56 +0800

    爬虫实战

--- 爬虫学习/爬虫项目/爬取番茄小说男频排行(多页爬取）.py
+++ 爬虫学习/爬虫项目/爬取番茄小说男频排行(多页爬取）.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
+import re
 import time
 from bs4 import BeautifulSoup
 
@@ -14,9 +15,17 @@ options.add_argument(
         "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 driver=webdriver.Edge(options=options)
 kk_URL='https://fanqienovel.com/library/all/page_'
+b=1
 for x in range(1,100):
     url=kk_URL+str(x)
     driver.get(url)
-    time.sleep(30)
+    time.sleep(6)
     y=driver.page_source
+    mou_shi=r'H">(.*?)</'
+    f=re.findall(mou_shi,y)
+    for c in f:
+        print(f'{b},{c}')
+        b=b+1
 
+'''嗯，分页爬取是没有问题的，因为确实自动爬取到了不同分页的内容，所以值得高兴。
+但是爬取的信息并不完整，因为番茄中文网用了一种名为“字体加密”的反爬手段。'''
