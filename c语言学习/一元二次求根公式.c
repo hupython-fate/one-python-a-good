3f40a74d74f4d725b675a5a9bc0ec08bb66e7018
hu cheng jian <13766397710@139.com>
2025-10-09 18:25:10 +0800

    c代码

--- c语言学习/一元二次求根公式.c
+++ c语言学习/一元二次求根公式.c
@@ -4,19 +4,19 @@
 int main() {
     int resurt_1;
     resurt_1=2+10/8*3;
-    printf("resurt_1：%d\n",resurt_1);
+    printf("resurt_1：%d\n",resurt_1);//int类型的占位符是%d
     double resurt_2;
     resurt_2=2+10/8.0*3;
-    printf("resurt_2:%lf\n",resurt_2);
-    //double yi_yuan_er_ci;
-    //yi_yuan_er_ci=-b+sqrt(b**2-4*a*c);//2a;
+    printf("resurt_2:%lf\n",resurt_2);//double类型的占位符是%lf
+    //float类型的占位符是%f
+    //char类型的占位符是%c
     int a=-1;
     int b=3;
     int c=5;
     double gen;
-    gen=-b+sqrt(pow(b,2)-4*a*c)/2*a;
+    gen=(-b+sqrt(pow(b,2)-4*a*c))/2*a;
     double gen_2;
-    gen_2=-b-sqrt(pow(b,2)-4*a*c)/2*a;
+    gen_2=(-b-sqrt(pow(b,2)-4*a*c))/2*a;
     printf("gen:%lf和%lf",gen,gen_2);
     return 0;
 
