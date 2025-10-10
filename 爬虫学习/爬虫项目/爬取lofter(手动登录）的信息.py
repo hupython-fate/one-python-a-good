f1c8eb5fef270ee00ddd7b366ea3b7a07174c031
hu cheng jian <13766397710@139.com>
2025-10-10 19:30:37 +0800

    爬虫的一次进步

--- 爬虫学习/爬虫项目/爬取lofter(手动登录）的信息.py
+++ 爬虫学习/爬虫项目/爬取lofter(手动登录）的信息.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 import time
 options=Options()
 # 反检测配置
@@ -18,19 +17,44 @@ driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () =>
 url="https://www.lofter.com/tag/%E8%AF%A1%E7%A7%98%E4%B9%8B%E4%B8%BBoc"
 driver.get(url)
 
-phone_input = driver.find_element(By.NAME, "phone")
-phone_input.send_keys("你的手机号")
+phone_input = driver.find_element(By.TAG_NAME, "input")
+phone_input.send_keys("13766397710")
+time.sleep(3)
+fa_song=driver.find_elements(By.NAME,'获取验证码')
+#<div class="EuoSRSUNPbbLCCRQmAzIfQ== " aria-hidden="true">获取验证码</div>
+#<div class="EuoSRSUNPbbLCCRQmAzIfQ== " aria-hidden="true">获取验证码</div>
+for b in fa_song:
+     b.click()#发送验证码
 
-time.sleep(60)
+#点击获取验证码后，会弹出一个验证窗口，目前这个问题还不知何解决。
+#虽然手机验证码登录问题还是没有解决，但是send_keys()和click()方法已为我开启了一扇新的门。自动化操控浏览器的门。
+
+tong=driver.find_element(By.TAG_NAME,'circle')
+tong.click()#点击同意用户协议
+
+#<button type="button" class="AaRSlfX5LcBgEqyHokS0hg== IWKkWSqHHfy16K39b0EUJQ== kbiv8yHlefNEThxFhRrfBQ==  hWICwaWdoLhCggGxnBgfhA==">注册/登录</button>
+lll=driver.find_element(By.TAG_NAME,'input')
+#<input type="text" placeholder="请输入验证码" class="vFxkt-gwu3ZAU3IZDjRQAA== ChtAyXVasJoZuqW5nzuRlQ== " autocomplete="off" maxlength="6" value="">
+time.sleep(30)
+x=input('请尽快输入验证码：')
+lll.send_keys(x)#输入获取的验证码
+
+gggg=driver.find_element(By.CLASS_NAME,'AaRSlfX5LcBgEqyHokS0hg== IWKkWSqHHfy16K39b0EUJQ== kbiv8yHlefNEThxFhRrfBQ==  hWICwaWdoLhCggGxnBgfhA==')
+#<button type="button" class="AaRSlfX5LcBgEqyHokS0hg== IWKkWSqHHfy16K39b0EUJQ== kbiv8yHlefNEThxFhRrfBQ==  hWICwaWdoLhCggGxnBgfhA==">注册/登录</button>
+gggg.click()#点击登录按钮。
+
+
+
+
 html=driver.page_source
-soup=BeautifulSoup(html,'html,parser')
-l=soup.find_all('h2')
-book_title=[]
-for v in l:
-    book_title.append(v.string)
-f=str(book_title)
-with open('./在lofter爬取的诡秘同人文书名.txt','w',encoding='utf-8') as g:
-    g.write(f)
+#soup=BeautifulSoup(html,'html,parser')
+#l=soup.find_all('h2')
+#book_title=[]
+#for v in l:
+   # book_title.append(v.string)
+#f=str(book_title)
+#with open('./在lofter爬取的诡秘同人文书名.txt','w',encoding='utf-8') as g:
+#    g.write(f)
 
 
 
