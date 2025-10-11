702234b2b3132d0363fbac49942286f03603a4b1
hu cheng jian <13766397710@139.com>
2025-10-12 00:08:38 +0800

    今天又是一个大进步，完成了人生中第一张图片的爬取。还有，无论是文本，还是音频，或是照片，本质上都是二进质文件。

---
+++ 爬虫学习/来都来了，总得爬点什么再走(成功了！爬取一张图片).py
@@ -0,0 +1,28 @@
+
+import requests.models as models
+import requests.sessions as sessions
+
+
+def pa_yi():
+    '''目标是爬取一张图片'''
+    req=models.Request()
+    req.method='GET'
+    req.headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+    }
+    req.url='https://img1.baidu.com/it/u=2651128509,3759062648&fm=253&fmt=auto&app=138&f=JPEG '
+    resp=req.prepare()
+    session=sessions.Session()
+    response=session.send(resp)
+
+    if response.status_code==200:
+        with open('./一张图片.jpg','wb') as f:
+            f.write(response.content)
+    else:
+        print(response.status_code)
+
+pa_yi()
